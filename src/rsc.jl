4429e0cbd1f35f392a41e4373a73638925d7a82f
Andrew Zheng <atz@Andrews-MacBook-Pro.local>
2022-02-08 09:52:20 -0500

    add solver option for rsc

--- src/rsc.jl
+++ src/rsc.jl
@@ -8,7 +8,8 @@ end
 reconstruct(U, S, V) = U * diagm(S) * V'
 
 function RSC(O::Array{S, 2}, Z::Array{S, 2};
-             λ=0., μ=0., r=Inf, Tpre=nothing) where S <: Real
+             λ=0., μ=0., r=Inf, Tpre=nothing,
+             solver=Ipopt.Optimizer) where S <: Real
     is_control, Tpre_inferred = infer_controls(Z)
     Tpre = Tpre !== nothing ? Tpre : Tpre_inferred
     U, Σ, V = SVT(O[is_control, :]; μ=μ, r=r)
@@ -16,7 +17,7 @@ function RSC(O::Array{S, 2}, Z::Array{S, 2};
     Y1pre = vec(mean(O[.!is_control, 1:Tpre]; dims=1))
     _, β = constrained_elastic_net(
         collect(Mhat[:, 1:Tpre]'), Y1pre;
-        α=0., λ=λ, intercept=false)
+        α=0., λ=λ, intercept=false, solver=solver)
 
     # Get timesteps where all treatment units were treated
     Ttreat = vec(all(Z[.!is_control, :] .== 1; dims=1))
